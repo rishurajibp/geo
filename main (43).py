fae157f23eb89a6564557f67f85e6fc8f16a7913
rishurajibp <rishurajibp4950@gmail.com>
2025-03-10 00:18:07 +0530

    Update main (43).py

--- main (43).py
+++ main (43).py
@@ -400,7 +400,7 @@ async def account_login(bot: Client, m: Message):
                             filename = res_file
                             await prog.delete(True)
                             start_time = time.time()
-                            await helper.send_vid(bot, m, cc, filename, thumb, name, prog, start_time)
+                            await helper.send_vid(bot, m, cc, filename, name, prog, start_time)
                             count += 1
                             time.sleep(4)
                         except Exception as e:
